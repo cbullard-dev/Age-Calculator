04960c6aeabe0a1d5282f29d8de08e84162072b2
Cal <60569006+cbullard-dev@users.noreply.github.com>
2025-09-17 10:29:58 +0900

    Initial Commit

---
+++ Age Calculator/Age_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Age_CalculatorApp.swift
+//  Age Calculator
+//
+//  Created by Cal on 2025/09/17.
+//
+
+import SwiftUI
+
+@main
+struct Age_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Age CalculatorTests/Age_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Age_CalculatorTests.swift
+//  Age CalculatorTests
+//
+//  Created by Cal on 2025/09/17.
+//
+
+import Testing
+@testable import Age_Calculator
+
+struct Age_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
