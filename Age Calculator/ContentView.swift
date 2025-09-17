6adf80e97947631e7fea2968518dec7f81c0ef8a
Cal <60569006+cbullard-dev@users.noreply.github.com>
2025-09-17 14:31:33 +0900

    This is my first iOS application created using the information taught in the "Building Your First iOS 17 App" on LinkedIn Learning.

--- Age Calculator/ContentView.swift
+++ Age Calculator/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var dateOfBirth: Date = Date()
+    var today:Date = Date()
+    @State var age: Double = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Image(systemName: "birthday.cake.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                    .font(.title)
+                Text("Age Calculator")
+                    .font(.title)
+            }.padding()
+            HStack{
+                Text("Enter Date of Birth:").font(.title2)
+            }.padding()
+            HStack{
+                DatePicker("",
+                           selection: $dateOfBirth,
+                           displayedComponents: [.date]).datePickerStyle(.wheel).padding()
+            }
         }
-        .padding()
+        HStack{
+            Text("Your age is: \(calculateAge(secondsSinceDate: today.timeIntervalSince(dateOfBirth)))")
+        }.padding()
+        if today.timeIntervalSince(dateOfBirth) < 0{
+            HStack{
+                Text("Please enter a date in the past!").font(.callout)
+            }
+        }
+        
+    }
+    
+    func calculateAge(secondsSinceDate:Double) -> Int{
+        let years:Int = Int(floor(secondsSinceDate / 31556952))
+        if years < 1{
+            return 0
+        }else{
+            return years
+        }
     }
 }
 
